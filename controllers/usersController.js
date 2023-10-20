36228bc82df3dccea4454c13884658686fe65e26
Berkay Şahin <berkaynihas@gmail.com>
2023-10-20 21:24:57 +0300

    user actions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -38,25 +38,66 @@ const createNewUser = asyncHandler(async (req, res) => {
     }
 })
 
-// router
-//     .route("/:id")
-//     .get((req, res) => {
-//         res.send(`Get user with id ${req.params.id}`)
-//     })
-//     .put((req, res) => {
-//         res.send(`Update user with id ${req.params.id}`)
-//     })
-//     .delete((req, res) => {
-//         res.send(`Delete user with id ${req.params.id}`)
-//     })
+const getOneUser = asyncHandler(async (req, res) => {
+    
+})
 
-// router.param("id", (req, res, next, id) => {
-//     next()
-// })
+const updateUser = asyncHandler(async (req, res) => {
+    const {id, username, password} = req.body
 
+    //does the user exist to update
+    const user = await User.findById(id).exec()
+
+    if (!user) {
+        return res.status(400).json({message: "User not found"})
+    }
+
+    //check for duplicate
+    const duplicate = await User.findById({username}).lean().exec()
+
+    //allow updates to the original user
+    if(duplicate && duplicate._id.toString() !== id) {
+        return res.status(409).json({message: "Duplicate username"})
+    }
+
+    user.username = username
+
+    if (password) {
+        user.password = await bcrypt.hash(password, 10)
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({message: `${updatedUser.username} updated`})
+
+})
+
+const deleteUser = asyncHandler(async (req, res) => {
+    const {id} = req.body
+
+    //confirming data
+    if (!id) {
+        res.status(400).json({message: "User ID required"})
+    }
+
+    //does the user exist
+    const user = await User.findById(id).exec()
+
+    if(!user) {
+        res.status(400).json({message: "User not found"})
+    }
+
+    const result = await user.deleteOne()
+
+    const reply = `Username ${reply.username} with ID ${reply._id} deleted`
+
+    res.json(reply)
+})
 //express goes top to bottom so any dynamic route should be located below a static one
 
 module.exports = {
     getAllUsers,
-    createNewUser
+    createNewUser,
+    updateUser,
+    deleteUser
 }
---
+++ models/Todo.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const todoSchema = new mongoose.Schema({
+    todo: {
+        type: String,
+        required: true
+    },
+})
